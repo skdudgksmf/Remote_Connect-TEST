4b7c754ac1dd09ca65d9079542a2afdfb60cb28f
skdudgksmf <skdudgksmf@naver.com>
2015-12-30 09:26:48 +0900

    added print line

--- Hello Git/src/com/sist/HelloGit.java
+++ Hello Git/src/com/sist/HelloGit.java
@@ -2,6 +2,7 @@ package com.sist;
 
 public class HelloGit {
  public static void main (String[] args){
-  System.out.println("Hello Git!");	   
+  System.out.println("Hello Git!");	 
+  System.out.println("What's git?");
    }
 }
